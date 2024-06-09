2fbbadc40d1ae4a44a2874a10a1a1a6c494cdef5
LeoneCarpine <162748876+LeoneCarpine@users.noreply.github.com>
2024-06-09 18:21:21 -0300

    Update funcoes.cpp

--- funcoes.cpp
+++ funcoes.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include "Classes.h"
 
+using namespace std;
+
+/*-----------------------------------------Personagens-----------------------------------------------*/
+
+Personagem::Personagem()
+{  
+    id = -1;
+    nome = " ";
+}
+
+Personagem::Personagem (int id, string nome)
+{
+    this->id = id;
+    this->nome = nome;
+}
+
+int Personagem::getId()
+{
+    return id;
+}
+
+string Personagem::getNome()
+{
+    return nome;
+}
