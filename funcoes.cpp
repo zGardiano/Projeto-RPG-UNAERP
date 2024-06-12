83fde37955212d583c4cea4f54c71eba04180bbd
LeoneCarpine <162748876+LeoneCarpine@users.noreply.github.com>
2024-06-12 13:21:20 -0300

    Update funcoes.cpp

--- funcoes.cpp
+++ funcoes.cpp
@@ -9,20 +9,34 @@ Personagem::Personagem()
 {  
     id = -1;
     nome = " ";
+    forca = 0;
+    vida = 0;
+    classe = 0;
 }
 
-Personagem::Personagem (int id, string nome)
+Personagem::Personagem (int id, string nome, int vida, int forca, int classe)
 {
     this->id = id;
     this->nome = nome;
+    this->vida = vida;
+    this->forca = forca;
+    this->classe = classe;
 }
 
 int Personagem::getId()
 {
     return id;
 }
-
 string Personagem::getNome()
 {
     return nome;
+}
+int Personagem::getVida(){
+    return vida;
+}
+int Personagem::getForca(){
+    return forca;
+}
+int Personagem::getClasse(){
+    return classe;
 }
